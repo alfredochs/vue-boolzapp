d4b832db17fb1ee22d6414a336df7b88f4cab860
alfredochs <chanta.alfredo@gmail.com>
2021-11-04 15:48:37 +0100

    model 2 begin

--- milestone2/script.js
+++ milestone2/script.js
@@ -4,25 +4,111 @@ new Vue({
     data: {
         allUsers: [
             {
-                nome: "Michele",
+                name: "Michele",
                 avatar: "img/avatar_1.jpg",
-                messaggio: "Messaggio 1"
+                visible: true,
+                messagges: [
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Ricordati di dargli da mangiare?",
+                        status: "sent"
+                    },
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Tutto Fatto!",
+                        status: "received"
+                    }
+                ]
             },
             {
-                nome: "Fabio",
+                name: "Fabio",
                 avatar: "img/avatar_2.jpg",
-                messaggio: "Messaggio 2"
+                visible: true,
+                messagges: [
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Ciao come stai?",
+                        status: "sent"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Bene grazie! Stasera ci vediamo?",
+                        status: "received"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Mi piacerebbe ma devo andare a fare la spesa",
+                        status: "received"
+                    },
+                ]
             },
             {
-                nome: "Samuele",
+                name: "Samuele",
                 avatar: "img/avatar_3.jpg",
-                messaggio: "Messaggio 3"
+                visible: true,
+                messagges: [
+
+                    {
+                        date: "28/03/2020 16:15:22",
+                        text: "La Marianna va in campagna",
+                        status: "received"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Sicuro di non aver sbagliato chat?",
+                        status: "sent"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Ah scusa!",
+                        status: "received"
+                    },
+                ]
             },
             {
-                nome: "Luisa",
+                name: "Luisa",
                 avatar: "img/avatar_4.jpg",
-                messaggio: "Messaggio 4"
+                visible: true,
+                messagges: [
+
+                    {
+                        date: "10/01/2020 15:50:00",
+                        text: "Lo sai che ha aperto una nuova pizzeria?",
+                        status: "sent"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Si, ma preferirei andare al cinema",
+                        status: "received"
+                    }
+                ]
             },
-        ]
+        ],
+        chatCliccata: null
+
+    },
+    methods: {
+        chatSelected(chiaviDellaChat) {
+            this.chatCliccata = chiaviDellaChat;
+            console.log(this.chatCliccata);
+        },
+
+        /**
+         * Creo la funzione per verificare che ci sia almeno un messaggio
+         *  e per poter stampara l'ultimo messaggio
+         * Passo come parametro l'array dentro allUsers cioè "messagges"
+         */
+        getMessage(messaggi) {
+            // messaggi è un array quindi faccio un controllo se c'è almeno un messaggio
+            if (messaggi.length === 0) {
+                return "Nessun Messaggio";
+            }
+            return messaggi[messaggi.length - 1].text;
+        }
     }
 });
