8a17938e0b66e54e306c5d861f95afcb18028a78
alfredochs <chanta.alfredo@gmail.com>
2021-11-05 14:52:45 +0100

    milestone 4

---
+++ milestone4/script.js
@@ -0,0 +1,140 @@
+Vue.config.devtools = true;
+new Vue({
+    el: "#root",
+    data: {
+        allUsers: [
+            {
+                name: "Michele",
+                avatar: "img/avatar_1.jpg",
+                visible: true,
+                messagges: [
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Hai portato a spasso il cane?",
+                        status: "sent"
+                    },
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Ricordati di dargli da mangiare!",
+                        status: "sent"
+                    },
+                    {
+                        date: "10/01/2020 16:15:22",
+                        text: "Tutto Fatto!",
+                        status: "received"
+                    }
+                ]
+            },
+            {
+                name: "Fabio",
+                avatar: "img/avatar_2.jpg",
+                visible: true,
+                messagges: [
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Ciao come stai?",
+                        status: "sent"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Bene grazie! Stasera ci vediamo?",
+                        status: "received"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Mi piacerebbe ma devo andare a fare la spesa",
+                        status: "received"
+                    },
+                ]
+            },
+            {
+                name: "Samuele",
+                avatar: "img/avatar_3.jpg",
+                visible: true,
+                messagges: [
+
+                    {
+                        date: "28/03/2020 16:15:22",
+                        text: "La Marianna va in campagna",
+                        status: "received"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Sicuro di non aver sbagliato chat?",
+                        status: "sent"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Ah scusa!",
+                        status: "received"
+                    },
+                ]
+            },
+            {
+                name: "Luisa",
+                avatar: "img/avatar_4.jpg",
+                visible: true,
+                messagges: [
+
+                    {
+                        date: "10/01/2020 15:50:00",
+                        text: "Lo sai che ha aperto una nuova pizzeria?",
+                        status: "sent"
+                    },
+                    {
+                        date: "20/03/2020 16:35:00",
+                        text: "Si, ma preferirei andare al cinema",
+                        status: "received"
+                    }
+                ]
+            },
+        ],
+        chatCliccata: {},
+        inputText: "",
+
+    },
+    methods: {
+        chatSelected(chiaviDellaChat) {
+            this.chatCliccata = chiaviDellaChat;
+        },
+        /**
+         * Creo la funzione per verificare che ci sia almeno un messaggio
+         *  e per poter stampara l'ultimo messaggio
+         * Passo come parametro l'array dentro allUsers cioè "messagges"
+         */
+        getMessage(messaggi) {
+            // messaggi è un array quindi faccio un controllo se c'è almeno un messaggio
+            if (messaggi.length === 0) {
+                return "Nessun Messaggio";
+            }
+            return messaggi[messaggi.length - 1].text;
+        },
+        addMessagge() {
+            if (this.inputText === "") {
+                return;
+            }
+            this.chatCliccata.messagges.push(
+                {
+                    date: "ora",
+                    text: this.inputText,
+                    status: "sent"
+                }
+            );
+            this.inputText = "";
+
+            setTimeout(() => {
+                this.chatCliccata.messagges.push(
+                    {
+                        date: "ora",
+                        text: "ok",
+                        status: "received"
+                    }
+                );
+            }, 1000);
+        }
+    },
+    mounted() {
+        this.chatCliccata = this.allUsers[0];
+    }
+
+});
